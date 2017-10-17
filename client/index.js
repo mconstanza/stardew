e58362d75a52f3380a8c6d8b2e56d95ff379252c
mconstanza <michael.constanza@gmail.com>
2017-10-17 10:09:29 -0400

    basic boilerplate set up

---
+++ client/index.js
@@ -0,0 +1,8 @@
+// ./client/index.js
+// Entry file
+
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDom.render(<App/>, document.getElementById('root'));
